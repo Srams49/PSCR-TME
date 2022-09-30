6392ab6f87fdef331fe7fbb88162f2b75f62f728
RAMS <28605296@ppti-24-303-12.ufr-info-p6.jussieu.fr>
2022-09-30 12:42:34 +0200

    tme2

--- TME2/tme2/hash.h
+++ TME2/tme2/hash.h
@@ -28,34 +28,46 @@ namespace pr {
         public:
             HashMap(size_t taille = 11001):seau_(taille),size_(taille){}
 
-        Valeur* getVal(const Clef &key){
-            size_t couper = std::hash<Clef>()(key);
-            size_t modul = couper%seau_.size();
-            for(Entry &e:seau_[modul]){
-                if(e.key == key){
-                    return &e.val;
+            Valeur* getVal(const Clef &key){
+                size_t couper = std::hash<Clef>()(key);
+                size_t modul = couper%seau_.size();
+                for(Entry &e:seau_[modul]){
+                    if(e.key == key){
+                        return &e.val;
+                    }
                 }
+                return nullptr;
             }
-            return nullptr;
-        }
 
-        bool put(const Clef &key, const Valeur &val){
-            size_t couper = std::hash<Clef>()(key);
-            size_t modul = couper%seau_.size();
-            for(Entry &e:seau_[modul]){
-                if(e.key == key){
-                    e.val = val;
-                    return true;
+            bool put(const Clef &key, const Valeur &val){
+                size_t couper = std::hash<Clef>()(key);
+                size_t modul = couper%seau_.size();
+                for(Entry &e:seau_[modul]){
+                    if(e.key == key){
+                        e.val = val;
+                        return true;
+                    }
                 }
+                size_ ++;
+                seau_[modul].emplace_front(key,val);
+                return false;
             }
-            size_ ++;
-            seau_[modul].emplace_front(key,val);
-            return false;
-        }
 
-        size_t getSize() const{
-            return size_;
-        }
+            size_t getSize() const{
+                return size_;
+            }
+
+            std::vector<std::pair<Clef,Valeur>> copieVector(){
+                std::vector<std::pair<Clef,Valeur>> v ;
+                v.reserve(getSize()); 
+                for (typename Table::iterator p = seau_.begin(); p != seau_.end(); p++ ){// Autre syntaxe for (auto(list:seau)){
+                    //for (const auto & e : *p)
+                    for(auto e = p->begin(); e != p->end(); e++){// for (auto(Entry:list)){
+                        v.push_back(std::pair<Clef,Valeur>(e->key,e->val));//p devient Entry
+                    }
+                }
+                return v;
+            }
             
     };
 }
